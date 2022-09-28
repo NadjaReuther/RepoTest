3ff0e9343eebeeed87b3365eac074c1266c79bc7
nadja.reuther <nadja_reuther@freenet.de>
2022-09-28 10:45:46 +0200

    2 Buttons eingefügt

--- RepoTest/Form1.cs
+++ RepoTest/Form1.cs
@@ -16,5 +16,10 @@ namespace RepoTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //erster Test
+        }
     }
 }
