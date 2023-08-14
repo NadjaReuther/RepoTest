35e1c5f5b3a4f28d4f8e1aa03d2c98f3cbbf9a4c
NadjaReuther <nadja_reuther@freenet.de>
2023-08-14 10:26:32 +0200

    Abschicken verschoben

--- RepoTest/Form1.Designer.cs
+++ RepoTest/Form1.Designer.cs
@@ -39,7 +39,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(133, 205);
+            this.button1.Location = new System.Drawing.Point(210, 212);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(297, 65);
             this.button1.TabIndex = 0;
